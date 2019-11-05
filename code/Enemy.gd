a083c4d07ffd7d14b40847e275e7a8e46e916750
Tim <tlawrencesteen@gmail.com>
2019-11-05 15:18:35 -0500

    add enemy scene
    on body enter reset player position

---
+++ code/Enemy.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Area2D_body_entered(body: PhysicsBody2D) -> void:
+	body.global_position.y = 500
+	pass # Replace with function body.
