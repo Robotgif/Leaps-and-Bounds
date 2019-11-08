99c7e3fd5eb00a5447b759c16d1e5dc67da7536e
Manuel Rodriguez <valle.mrv@gmail.com>
2019-11-08 09:14:58 +0100

    modify the movement limit

--- code/Player.gd
+++ code/Player.gd
@@ -11,6 +11,7 @@ export (int) var health = 100
 var _score = 0
 var _jump_speed_moment = jump_speed
 var _velocity = Vector2()
+var size_viewport = null
 
 func set_damage(damage):
 	health -= damage
@@ -23,12 +24,16 @@ func get_score():
 
 func get_health():
 	return health	
+	
+func _ready():
+	size_viewport = get_node("Camera2D").get_viewport_rect().size
+	
 
 func _get_input():
+	_set_positon_about_visivilty_status()
 	_velocity.x = 0
 	var left = Input.is_action_pressed("move_left")
 	var right = Input.is_action_pressed("move_right")
-	
 	if is_on_floor():
 		_velocity.y = _jump_speed_moment
 		set_collision_mask_bit(4, false)
@@ -44,3 +49,8 @@ func _physics_process(delta):
 	_velocity.y += gravty * delta	
 	_velocity = move_and_slide(_velocity, Vector2(0, -1))
 
+func _set_positon_about_visivilty_status():
+	if position.x < 0:
+		position.x = size_viewport.x + position.x
+	elif position.x > size_viewport.x:
+		position.x = 0 + position.x - size_viewport.x
